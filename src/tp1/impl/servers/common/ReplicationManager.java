db21ee8e91fccc76d611c6a4ef38ec52a0073ff7
Alexandre Oliveira <91698340+nedzer0@users.noreply.github.com>
2022-06-05 10:41:04 +0100

    Update ReplicationManager.java

--- src/tp1/impl/servers/common/ReplicationManager.java
+++ src/tp1/impl/servers/common/ReplicationManager.java
@@ -1,6 +1,6 @@
 package tp1.impl.servers.common;
 
-import java.util.LinkedList;
+
 import java.util.List;
 import kafka.KafkaPublisher;
 import kafka.KafkaSubscriber;
@@ -9,7 +9,7 @@ import kafka.sync.SyncPoint;
 
 public class ReplicationManager {
 
-	static final String TOPIC = "mytopic";
+	static final String TOPIC = "single_partition_topic";
 	static final String KAFKA_BROKERS = "kafka:9092";
 	static final String REPLAY_FROM_BEGINNING = "earliest";
 	private KafkaPublisher kp;
@@ -20,10 +20,9 @@ public class ReplicationManager {
 	}
 
 	private void start(JavaRepDirectory rep) {
-		List<String> topics = new LinkedList<>();
-		topics.add(TOPIC);
+
 		kp = KafkaPublisher.createPublisher(KAFKA_BROKERS);
-		ks = KafkaSubscriber.createSubscriber(KAFKA_BROKERS, topics, REPLAY_FROM_BEGINNING);
+		ks = KafkaSubscriber.createSubscriber(KAFKA_BROKERS, List.of(TOPIC), REPLAY_FROM_BEGINNING);
 		ks.start(false, new ProcessOperation(rep));
 	}
 
